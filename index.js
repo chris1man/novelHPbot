15089e7115f5fdfbe1da945528a6d104271756ff
chris1man <limajchris@gmail.com>
2026-02-01 16:27:33 +0700

    test

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-require('dotenv').config();
-const { Telegraf } = require('telegraf');
 const express = require('express');
-
-const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
 
 const PORT = process.env.PORT;
+
 if (!PORT) {
     console.error('PORT is not defined');
     process.exit(1);
 }
 
-app.use(express.json());
-
 app.get('/', (req, res) => {
-    res.send('APP IS ALIVE');
+    res.status(200).send('EXPRESS ONLY — WORKS');
 });
-
-app.post('/', (req, res) => {
-    bot.handleUpdate(req.body);
-    res.sendStatus(200);
-});
-
-bot.start((ctx) => ctx.reply('Ура! Я наконец-то заработал! 🚀'));
-bot.on('text', (ctx) => ctx.reply(`Эхо: ${ctx.message.text}`));
-
-(async () => {
-    await bot.telegram.setWebhook(`https://chris1man-novelhpbot-df74.twc1.net/`);
-    console.log('Webhook set');
-})();
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
